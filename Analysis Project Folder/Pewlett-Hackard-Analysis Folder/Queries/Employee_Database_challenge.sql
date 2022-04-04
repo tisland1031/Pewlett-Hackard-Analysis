b10f352da470eafa595316b5ecb4505e88d7398d
tisland1031 <96746207+tisland1031@users.noreply.github.com>
2022-04-03 18:57:44 -0700

    Update Employee_Database_challenge.sql

--- Analysis Project Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
+++ Analysis Project Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -51,3 +51,30 @@ WHERE (de.to_date = '9999-01-01')
 AND (e.birth_date BETWEEN '1965_01_01' AND '1965-12-31')
 ORDER BY emp_no ASC;
 
+--Additional queries for Readme Summary 
+--Retrieve the number of employees by their  job title who are eligibale for mentorship program.
+SELECT COUNT(me.emp_no),
+me.title
+--INTO mentorship_eligibilty_titles
+FROM mentorship_eligibilty as me
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
+
+--Additional queries for readme
+SELECT DISTINCT ON (emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+--INTO mentorship_eligibilty
+FROM employees AS e
+	JOIN dept_emp AS de 
+		ON (e.emp_no = de.emp_no)
+	JOIN titles AS t
+		ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01') 
+AND (e.birth_date BETWEEN '1964_01_01' AND '1965-12-31')
+ORDER BY emp_no ASC;
+
